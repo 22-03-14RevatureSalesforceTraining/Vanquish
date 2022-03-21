6a5b3e6c573300ec99eceb30d99890167032ee00
Alberto Vergara <alberto456@revature.net>
2022-03-20 23:50:21 -0600

    minor error fix

--- AlbertoJS.js
+++ AlbertoJS.js
@@ -1,10 +1,15 @@
-const request = new XMLHttpRequest();
-request.open("GET", "https://www.delish.com/cooking/recipe-ideas/a23014857/classic-stuffed-peppers-recipe/");
-request.send();
-request.onload = () => {
-    if (request.status === 200) {
-        console.log(JSON.parse(request.response));
-    } else {
-        console.log(`error ${request.status}`)
+/* API */
+try {
+    const request = new XMLHttpRequest();
+    request.open("GET", "https://www.delish.com/cooking/recipe-ideas/a23014857/classic-stuffed-peppers-recipe/");
+    request.send();
+    request.onload = () => {
+        if (request.status === 200) {
+            console.log(JSON.parse(request.response));
+        } else {
+            console.log(`error ${request.status}`)
+        }
     }
+} catch (e) {
+    console.log("API not working");
 }
