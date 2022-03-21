79ca257668590a949f5d6d064bb7e57fba9e9734
Alberto Vergara <alberto456@revature.net>
2022-03-21 13:37:33 -0600

    updated button and changed background to better suit text

--- AlbertoJS.js
+++ AlbertoJS.js
@@ -1,7 +1,41 @@
+/*Button to change ingredients table*/
+quantities = [4, 1, 2, 1, 1, 1, 1, 3 / 4];
+var quantity1 = quantities[0]*2;
+var quantity2 = quantities[1]*2;
+var quantity3 = quantities[2]*2;
+var quantity4 = quantities[3]*2;
+var quantity5 = quantities[4]*2;
+var quantity6 = quantities[5]*2;
+var quantity7 = quantities[6]*2;
+var quantity8 = quantities[7]*2;
+
+window.onload = function () {
+    document.getElementById("changeRecipeButton").addEventListener("click", buttonPress);
+}
+
+
+function buttonPress() {
+    try {
+        document.getElementById('quantityOne').innerHTML = quantity1;
+        document.getElementById('quantityTwo').innerHTML = quantity2;
+        document.getElementById('quantityThree').innerHTML = quantity3;
+        document.getElementById('quantityFour').innerHTML = quantity4;
+        document.getElementById('quantityFive').innerHTML = quantity5;
+        document.getElementById('quantitySix').innerHTML = quantity6;
+        document.getElementById('quantitySeven').innerHTML = quantity7;
+        document.getElementById('quantityEight').innerHTML = quantity8;
+    } catch (e) {
+        console.log("This didn't work");
+    } finally {
+        console.log("Try-Catch block has ended");
+    }
+}
+
+
 /* API */
 try {
     const request = new XMLHttpRequest();
-    request.open("GET", "https://www.delish.com/cooking/recipe-ideas/a23014857/classic-stuffed-peppers-recipe/");
+    request.open("GET", "https://mocki.io/v1/97e350e2-7023-4d2a-983e-9190a36f6770");
     request.send();
     request.onload = () => {
         if (request.status === 200) {
@@ -12,4 +46,6 @@ try {
     }
 } catch (e) {
     console.log("API not working");
+} finally {
+    console.log("API has finished");
 }
